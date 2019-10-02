f9ae6bd31b2312b2852ee973655073adc973a2d2
WinembergJo <jwine@knights.ucf.edu>
2019-10-02 10:01:41 -0400

    Project Created
    The initial Unreal Engine project was created and partially setup.

---
+++ Mayday/Source/Mayday/MaydayGameModeBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "MaydayGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MAYDAY_API AMaydayGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
